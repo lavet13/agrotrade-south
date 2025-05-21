714c590d2b6dd2f8e5a94f20c83d35265b3561c3
lavet13 <lavet13@mail.ru>
2025-05-21 17:05:40 +0300

    support dark mode for icons

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -639,7 +639,7 @@ function Index() {
                           fillRule="evenodd"
                           clipRule="evenodd"
                           className={
-                            "self-center w-[22px] h-[22px] fill-gold-500 transition-colors"
+                            "self-center w-[22px] h-[22px] fill-gold-500 dark:fill-gold-200 transition-colors"
                           }
                         >
                           <path d="M166667 0c92048 0 166667 74619 166667 166667s-74619 166667-166667 166667S0 258715 0 166667 74619 0 166667 0zm80219 91205l-29735 149919s-4158 10396-15594 5404l-68410-53854s76104-68409 79222-71320c3119-2911 2079-3534 2079-3534 207-3535-5614 0-5614 0l-100846 64043-42002-14140s-6446-2288-7069-7277c-624-4992 7277-7694 7277-7694l166970-65498s13722-6030 13722 3951zm-87637 122889l-27141 24745s-2122 1609-4443 601l5197-45965 26387 20619z" />
@@ -650,7 +650,7 @@ function Index() {
                         href="https://wa.me/+79784661456"
                       >
                         <svg
-                          className={"self-center w-[22px] h-[22px] fill-gold-500 transition-colors"}
+                          className={"self-center w-[22px] h-[22px] fill-gold-500 dark:fill-gold-200 transition-colors"}
                           version="1.1"
                           id="Layer_1"
                           xmlns="http://www.w3.org/2000/svg"
